c78d0e2ce3a46686575a527c892e8f3477113b07
futuroDevLeo <leonunes07@outlook.com>
2024-04-27 15:07:07 -0300

    feat(HELLO-WORLD.PY): with continuous study of python make two more different forms to say hello world

--- hello-world.py
+++ hello-world.py
@@ -1,2 +1,13 @@
-# 1 With only a log
-print("Hello World")
+# 1 With only a print
+print("1. Hello World")
+
+# 2 With only a variable
+message = '2. Hello World'
+print(message)
+
+# 3 With a function
+def greetings():
+    name = input('What is your name? ')
+    return print(f'3. Hello World, {name}')
+
+greetings()
